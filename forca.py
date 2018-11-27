c3465709f6f6554b01a0a4a3ac073f1fa99be2e4
Samuel Xavier <samuelxavier@univicosa.com.br>
2018-11-27 15:26:37 -0200

    adicionada primeira desenvolvimento da forca

--- forca.py
+++ forca.py
@@ -3,6 +3,28 @@ def jogar():
     print("***Bem vindo ao jogo de Forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    # enquanto não False E não False
+    while (not acertou and not enforcou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
+
+
+
+
 
 
     print("Fim do jogo")
